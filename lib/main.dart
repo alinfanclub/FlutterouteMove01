3068bd3ca45c449774cbb9212d95b2ed1312e4cf
e759ksh <e759ksh@naver.com>
2022-05-12 19:32:48 +0900

    route

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:one/screenA.dart';
+import 'package:one/screenB.dart';
+import 'package:one/screenC.dart';
+// import 'screenA.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => screenA(),
+        '/b': (context) => screenB(),
+        '/c': (context) => screenC(),
+      },
+      // home: ScreenA(),
+    );
+  }
+}
---
+++ lib/screenC.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ScreenA());
+
+class ScreenA extends StatelessWidget {
+  const ScreenA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ScreenA",
+      color: Colors.green,
+      home: screenC(),
+    );
+  }
+}
+
+class screenC extends StatelessWidget {
+  const screenC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ScreenC"),
+        backgroundColor: Color.fromARGB(255, 204, 236, 42),
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "ScreenC",
+              style: TextStyle(fontSize: 20, color: Colors.orange),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
